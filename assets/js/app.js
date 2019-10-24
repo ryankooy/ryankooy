fc856a9eac32e43d0a3b016349515bb44ad3fe91
Ryan Kooy <rwkooy@gmail.com>
2019-10-24 11:57:41 -0400

    accordion animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,5 +4,6 @@ $(document).ready(function () {
   $('.enter').on('click', function () {
     $('.landing').addClass('uk-animation-fade uk-animation-reverse');
     $('.acc').show();
+    $('.acc').attr('uk-scrollspy', 'cls: uk-animation-fade; target: ul; delay: 1000; repeat: true');
   });
 });
