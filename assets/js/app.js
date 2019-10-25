8a495cf76b06539655557f8a34fa7ecd0527f807
Ryan Kooy <rwkooy@gmail.com>
2019-10-25 17:21:13 -0400

    bio improvement

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 
   $('.enter').on('click', function () {
     $('.landing').addClass('uk-animation-fade uk-animation-reverse');
+    $('.footer').addClass('uk-animation-fade uk-animation-reverse');
     $('.acc').show();
     $('.acc').attr('uk-scrollspy', 'cls: uk-animation-fade; target: ul; delay: 1000; repeat: true');
   });
