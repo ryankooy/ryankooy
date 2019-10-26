766b4108a45378b4b69801f158099e26aa319009
Ryan Kooy <rwkooy@gmail.com>
2019-10-25 23:31:22 -0400

    proficiency section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
-  $('.acc').hide();
+  $('.acc').addClass('uk-hidden');
+  // $('#spin').addClass('uk-hidden');
 
   $('.enter').on('click', function () {
     $('.landing').addClass('uk-animation-fade uk-animation-reverse');
     $('.footer').addClass('uk-animation-fade uk-animation-reverse');
-    $('.acc').show();
+    $('.acc').removeClass('uk-hidden');
     $('.acc').attr('uk-scrollspy', 'cls: uk-animation-fade; target: ul; delay: 1000; repeat: true');
   });
+
+  // $('.tit').on('click', function () {
+  //   $('#spin').removeClass('uk-hidden');
+  //   setTimeout(function () { $('#spin').addClass('uk-animation-fade'); }, 1000);
+  // });
 });
