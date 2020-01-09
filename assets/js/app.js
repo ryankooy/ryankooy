8da399930e18ab02a35b5ac3f0c7f223c678773c
Ryan Kooy <rwkooy@gmail.com>
2020-01-09 10:13:04 -0500

    resume update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,6 @@ $(document).ready(() => {
   $('.acc').addClass('uk-hidden');
 
   $('.enter').on('click', () => {
-    // $('.landing, .footer').addClass('uk-animation-fade uk-animation-reverse');
     $('.landing, .footer').hide();
     $('.acc').removeClass('uk-hidden');
     $('.acc').attr('uk-scrollspy', 'cls: uk-animation-fade; delay: 100');
