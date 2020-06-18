90bc710c43984e04cc29e34aae8a987ddff4c4f6
Ryan Kooy <rwkooy@gmail.com>
2020-06-18 10:50:08 -0400

    minor animation edit

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,7 @@ $(document).ready(() => {
   $('.enter').on('click', () => {
     $('.landing, .footer').hide();
     $('.acc').removeClass('uk-hidden');
-    $('.acc').attr('uk-scrollspy', 'cls: uk-animation-fade; delay: 100');
-    setTimeout(() => byeBG(), 500);
+    setTimeout(() => byeBG(), 550);
   })
 
   const byeBG = () => {
